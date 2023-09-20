a6635a67c9f556734dda6fa65a3a7ced5bd346da
yuhsuan <410335032@gms.ndhu.edu.tw>
2023-09-20 20:03:01 +0800

    update meerkat game

--- src/app/meerkat-game/meerkat-game.component.ts
+++ src/app/meerkat-game/meerkat-game.component.ts
@@ -11,7 +11,7 @@ export class MeerkatGameComponent {
   items: MeerkatGameItem[] = items;
   stageImgMap = stageImgMap;
   pageStages = pageStages;
-  currentStage = pageStages.gaming;
+  currentStage = pageStages.desc1;
 
   constructor(
   ) {
